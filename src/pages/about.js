c261a7509fa5ebe9022357ff5fa0e23698c7eaa4
John Doe <jack.h.sawyer@pm.me>
2024-08-22 20:09:08 -0500

    Added and about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
             {/* <h1>About Me</h1> */}
-            <p>Hi there! I'm the creator of this site, which I built with Gatsby.</p>
+            <p>Hi there! I’m the creator of this site, crafted with Gatsby. As a web developer and a single father to a teenage daughter, I lead a life that might seem ordinary to me, though I’m always envisioning greater possibilities. In reality, my journey has taken me around the world, allowing me to connect with remarkable individuals. Yet, my greatest pride lies in providing a loving and secure home for my daughter.</p>
             <Link to="/">Back to Home</Link>
         </Layout>
     )
